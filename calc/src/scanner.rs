5dd9bd65ff9a5cd9dac15b9b57734b914c643d28
Aggrathon <antonbjo@gmail.com>
2018-05-07 15:33:37 +0300

    Add functions and constants

--- calc/src/scanner.rs
+++ calc/src/scanner.rs
@@ -1,4 +1,8 @@
+extern crate natural_constants;
+extern crate rand;
+
 use std;
+use self::natural_constants::physics;
 
 #[derive(PartialEq)]
 pub enum Token {
@@ -7,10 +11,15 @@ pub enum Token {
 	Multiplication,
 	Division,
 	Power,
+	Factorial,
 	Number(f64),
 	END,
 	Unknown,
 	Text(String),
+	Function(Function),
+	Comma,
+	Lparen,
+	Rparen,
 }
 
 impl std::fmt::Display for Token {
@@ -25,8 +34,38 @@ impl std::fmt::Display for Token {
 			Token::Text(ref s) => write!(f, "Text: {}", s),
 			Token::END => write!(f, "END"),
 			Token::Unknown => write!(f, "Unknown"),
+			Token::Factorial => write!(f, "Operator: !"),
+			Token::Comma => write!(f, "Marker: ,"),
+			Token::Lparen => write!(f, "Marker: ("),
+			Token::Rparen => write!(f, "Marker: )"),
+			Token::Function(ref s) => write!(f, "Function: {}", s),
 		}
     }
+}
+
+#[derive(PartialEq)]
+pub enum Function {
+	Log,
+	Ln,
+	Abs,
+	Cos,
+	Sin,
+	Tan,
+	Sqrt,
+}
+
+impl std::fmt::Display for Function {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+			Function::Abs => write!(f, "abs"),
+			Function::Ln => write!(f, "ln"),
+			Function::Log => write!(f, "log"),
+			Function::Cos => write!(f, "cos"),
+			Function::Sin => write!(f, "sin"),
+			Function::Tan => write!(f, "tan"),
+			Function::Sqrt => write!(f, "sqrt"),
+		}
+	}
 }
 
 pub struct Scanner {
@@ -80,6 +119,12 @@ impl Scanner {
 			'-' => Token::Subtraction,
 			'/' => Token::Division,
 			'^' => Token::Power,
+			'!' => Token::Factorial,
+			',' => Token::Comma,
+			'(' => Token::Lparen,
+			')' => Token::Rparen,
+			'[' => Token::Lparen,
+			']' => Token::Rparen,
 			'*' => {	// ** == ^
 				let od = match self.iterator.peek() {
 					Option::None => return Token::Multiplication,
@@ -127,12 +172,38 @@ impl Scanner {
 						self.iterator.next();
 						self.index+= 1;
 					}
-					Token::Text(s)
+					Scanner::parse_text(s)
 				}
 				else {
 					self.get_next_token()
 				}
 			},
+		}
+	}
+
+	fn parse_text(s: String) -> Token {
+		let l = s.to_lowercase();
+		match l.as_ref() {
+			//Constants
+			"e" => Token::Number(std::f64::consts::E),
+			"pi" => Token::Number(std::f64::consts::PI),
+			"tau" => Token::Number(std::f64::consts::PI*2.0),
+			"sqrt2" => Token::Number(std::f64::consts::SQRT_2),
+			"deg2rad" => Token::Number(std::f64::consts::PI / 180.0),
+			"rad2deg" => Token::Number(std::f64::consts::FRAC_1_PI * 180.0),
+			"epsilon" => Token::Number(std::f64::EPSILON),
+			"c" => Token::Number(physics::speed_of_light_vac),
+			"rnd" => Token::Number(rand::random::<f64>()),
+			//Functions
+			"ln" => Token::Function(Function::Ln),
+			"log" => Token::Function(Function::Log),
+			"sin" => Token::Function(Function::Sin),
+			"cos" => Token::Function(Function::Cos),
+			"tan" => Token::Function(Function::Tan),
+			"abs" => Token::Function(Function::Abs),
+			"sqrt" => Token::Function(Function::Sqrt),
+			//Unknown
+			_ => Token::Text(s),
 		}
 	}
 }

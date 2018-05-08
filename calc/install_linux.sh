fe7b488678f2329790764a19d2d657a6ce67b464
Aggrathon <antonbjo@gmail.com>
2018-05-08 17:30:14 +0300

    install script

---
+++ calc/install_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Building the calculator:"
+cd `dirname "$0"`
+cargo build --release
+echo "Installing the executable to /usr/local/bin/calc:"
+sudo rm -f /usr/local/bin/calc
+sudo cp target/release/calc /usr/local/bin/calc

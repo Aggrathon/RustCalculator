a756d7caa42ffb39a6daf12b9dd67537d170422e
Aggrathon <antonbjo@gmail.com>
2018-10-23 15:25:41 +0300

    Fix local path

--- install_linux.sh
+++ install_linux.sh
@@ -2,7 +2,7 @@
 echo "Building the calculator:"
 cd `dirname "$0"`
 cargo build --release
-strip /target/release/calc
+strip target/release/calc
 echo "Installing the executable to /usr/local/bin/calc:"
 sudo rm -f /usr/local/bin/calc
 sudo cp target/release/calc /usr/local/bin/calc
